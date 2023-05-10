c2374fba9fd5a06cf7b8b85eef7ee4aac847f4ab
Александр Хохлов <94252054+Aleksandr-Hohlov@users.noreply.github.com>
2023-05-10 22:04:23 +0300

    Add files via upload

---
+++ precache-manifest.b1c10cfc2283c8fd0ac9e46c9d2b774f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "99ab50470b9caa7c477d",
+    "url": "/static/css/main.490f0fad.chunk.css"
+  },
+  {
+    "revision": "99ab50470b9caa7c477d",
+    "url": "/static/js/main.99ab5047.chunk.js"
+  },
+  {
+    "revision": "1fd78bf72b6aa9ef6d12",
+    "url": "/static/js/1.1fd78bf7.chunk.js"
+  },
+  {
+    "revision": "37d76c781b6a0d78a2ce",
+    "url": "/static/js/2.37d76c78.chunk.js"
+  },
+  {
+    "revision": "b1fe40465857bc4f99c2",
+    "url": "/static/js/runtime~main.b1fe4046.js"
+  },
+  {
+    "revision": "e38b395c190826b3bf6d78baff1415a0",
+    "url": "/static/media/imageSuccess.e38b395c.svg"
+  },
+  {
+    "revision": "4072126c596bd9485c6e9601f6d4d35d",
+    "url": "/static/media/imageError.4072126c.svg"
+  },
+  {
+    "revision": "66f0ef6196f8d81ced9930f6675035a1",
+    "url": "/static/media/Inter-Medium.66f0ef61.woff"
+  },
+  {
+    "revision": "d0b121f3a9d3d88afdfd6902d31ee9a0",
+    "url": "/static/media/Inter-Black.d0b121f3.woff"
+  },
+  {
+    "revision": "661569afe57a38e1529a775a465da20b",
+    "url": "/static/media/Inter-Black.661569af.woff2"
+  },
+  {
+    "revision": "3ac83020fe53b617b79b5e2ad66764af",
+    "url": "/static/media/Inter-Regular.3ac83020.woff"
+  },
+  {
+    "revision": "eea97ded621cd8a18067c7243b19ceea",
+    "url": "/static/media/Inter-Medium.eea97ded.woff2"
+  },
+  {
+    "revision": "eec224cbddb8500ac62361155b34f265",
+    "url": "/static/media/like.eec224cb.svg"
+  },
+  {
+    "revision": "3d813f810a41032c140568c878fd7d85",
+    "url": "/static/media/like_active.3d813f81.svg"
+  },
+  {
+    "revision": "6bfdb35467f51d3ba1f70f8d4a9c2b59",
+    "url": "/static/media/edit.6bfdb354.svg"
+  },
+  {
+    "revision": "cb0f2bbfd915a7bfa586c3a3bd2efc48",
+    "url": "/static/media/Close_Icon.cb0f2bbf.svg"
+  },
+  {
+    "revision": "dc131113894217b5031000575d9de002",
+    "url": "/static/media/Inter-Regular.dc131113.woff2"
+  },
+  {
+    "revision": "4831282e649d143ce022040beb49ad4f",
+    "url": "/static/media/Delete_Icon.4831282e.svg"
+  },
+  {
+    "revision": "5bae30e928077806f1bdb3549dd65d86",
+    "url": "/index.html"
+  }
+];
